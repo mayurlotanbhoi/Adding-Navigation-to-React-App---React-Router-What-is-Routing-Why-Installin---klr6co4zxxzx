4ab6c3f24a5901c8af15893c874154bedab8b2f3
mayur bhoi <mayurbhoi200@gmail.com>
2022-09-16 13:47:00 +0530

    updateCompo

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-import { BrowserRouter } from "react-router-dom";
-import { NavBar } from "./NavBar";
-import { LocationDisplay } from "./LocationDisplay";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import LocationDisplayComp from "./LocationDisplay";
+function About() {
+  return <div>You are on the about page.</div>;
+}
+function Home() {
+  return <div>You are home.</div>;
+}
 
-function App() {
-  return (
-    <div id="main">
-      {/* Do not remove the main div */}
+function Invalid() {
+  return <div>No match</div>;
+}
 
-      <BrowserRouter>
-        <LocationDisplay />
-        <NavBar />
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/" component={Invalid} />
+        </Switch>
+        <LocationDisplayComp />
+      </div>
+    );
+  }
 }
 
 export default App;
+export const LocationDisplay = LocationDisplayComp;
--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-export const LocationDisplay = () => {
+export default function LocationDisplayComp() {
   let location = useLocation();
-  // console.log(location)
   return <div data-testid="location-display">{location.pathname}</div>;
-};
-
-// export default LocationDisplay;
+}
